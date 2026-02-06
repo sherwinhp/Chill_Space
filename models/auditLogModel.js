bc79a5e8290ad7f092f3bcc4d9b038737dd45100
Javier Choo <24049188@myrp.edu.sg>
2026-02-06 22:55:15 +0800

    Compliance declaration

--- models/auditLogModel.js
+++ models/auditLogModel.js
@@ -1,3 +1,15 @@
+/*
+I declare that this code was written by me.
+I will not copy or allow others to copy my code.
+I understand that copying code is considered as plagiarism.
+
+Student Name: Angelo Miguel Beltran Casia, Aaron Ryan Tan Wei Rong, Chow Sherwin, Aniq Syazwan Bin Muliadi, Choo Tian En Javier, Christine Joy Teh Shi Hui
+
+Student ID: 24048278, 24045221, 24049188, 24048876, 24046565, 24048424
+
+Class: C372-002-E63C
+Date created: 06-02-2026
+*/
 const db = require("../db");
 
 async function addAuditLog({
